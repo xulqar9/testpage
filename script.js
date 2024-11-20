408c290cfbef62a7fa24cd8f457b2b419bf4cc80
xulqar9 <a.salloum@gmx.de>
2024-11-20 15:32:56 +0100

    improve page

--- script.js
+++ script.js
@@ -76,3 +76,9 @@ document.addEventListener("DOMContentLoaded", () => {
       }
   });
 });
+
+
+function toggleNav() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.toggle('open');
+}
